99bbac6a378ebb2d8e2bf0295087195073699f42
Nigarshaikh12 <nigar.shaikh@cumminscollege.in>
2016-08-09 14:47:38 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -2,9 +2,8 @@
 void main()
 {
   int a,b,c;
-  //Add calculator function here.
 }
-// This fiunction gives addition of two numbers
+// This function gives addition of two numbers
 int add(int p,int q)
 {
   return p+q;
